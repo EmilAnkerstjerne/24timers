9c216ad708343072c74d31a8ac0e076e34c1ada7
Emil Ankerstjerne <emilankerstjerne@MacBook-Pro-tilhrende-Emil.local>
2021-06-10 09:27:44 +0200

    first commit

---
+++ src/main/java/com/example/eksamen24timers/Eksamen24timersApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamen24timers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamen24timersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eksamen24timersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eksamen24timers/Eksamen24timersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksamen24timers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksamen24timersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
